a06db36de48e158e0050e75d3864663565bcd618
Troels Hune Gjørup <troels.hune.gjoerup@post.au.dk>
2022-05-02 18:10:01 +0200

    added form for streamCreate

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+import { configureStore, compose } from "@reduxjs/toolkit";
 import App from "./components/App";
 import reducers from "./reducers";
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = configureStore({
 	reducer: reducers,
+	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+	enhancers: composeEnhancers,
 });
 export default store;
 const container = document.getElementById("root");
