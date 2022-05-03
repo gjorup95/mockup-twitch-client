3691361a2bd6b1420412515ef9567ed67711ed2b
Troels Hune Gjørup <troels.hune.gjoerup@post.au.dk>
2022-05-03 12:22:34 +0200

    added edit stream functionality

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,6 +8,7 @@ import {
 	EDIT_STREAM,
 } from "./types";
 import streams from "../apis/streams";
+import history from "../history";
 
 export const signIn = (userId) => {
 	return {
@@ -28,6 +29,7 @@ export const createStream = (formValues) => async (dispatch, getState) => {
 	dispatch({ type: CREATE_STREAM, payload: response.data });
 	// DO SOME PROGRAMMATIC NAVIGATION
 	// MOVE THE USER TO HOME PAGE
+	history.push("/");
 };
 
 export const fetchStreams = () => async (dispatch) => {
@@ -41,8 +43,9 @@ export const fetchStream = (id) => async (dispatch) => {
 };
 
 export const editStream = (id, formValues) => async (dispatch) => {
-	const response = await streams.put(`/streams/${id}`, formValues);
+	const response = await streams.patch(`/streams/${id}`, formValues);
 	dispatch({ type: EDIT_STREAM, payload: response.data });
+	history.push("/");
 };
 
 export const deleteStream = (id) => async (dispatch) => {
--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -10,11 +10,13 @@ class StreamList extends React.Component {
 		if (stream.userId === this.props.currentUserId) {
 			return (
 				<div className="right floated content">
-					<button className="ui button primary">Edit</button>
-					<button className="ui button negative">Delete</button>
+					<Link to={`/streams/edit/${stream.id}`} className="ui button primary">
+						Edit
+					</Link>
+					<Link className="ui button negative">Delete</Link>
 				</div>
 			);
-		}
+		} // TODO: FIX SECOND LINK BUTTON!!!
 	}
 	renderCreateButton() {
 		if (this.props.isSignedIn) {
